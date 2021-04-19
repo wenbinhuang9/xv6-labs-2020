14bf476bae307fc00a083f9a83338ebf3726ea88
wenbinhuang9 <ben@wenbins-MacBook-Pro.local>
2021-04-18 18:47:16 -0700

    add trace sys call

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -8,6 +8,20 @@
 #include "proc.h"
 
 uint64
+sys_trace(void)
+{
+  int mask;
+
+  if(argint(0, &mask) < 0)
+    return -1;
+
+  struct proc *p = myproc();
+  p->tracemask = mask;
+
+  return 0;
+}
+
+uint64
 sys_exit(void)
 {
   int n;
